5901c450a9bdeec3b786b12ac870245b010f33b1
Yann Coleu <y@nn-col.eu>
2017-10-23 13:04:29 +0200

    Proxy refactoring
    Make the proxy more robust than before:
    
    - URL routes are better handled
    - Auth and Logs as middleware
    - Better error handling
    - Easy config
    - Add version route

--- go_graphite_proxy.go
+++ go_graphite_proxy.go
@@ -1,103 +1,202 @@
 package main
 
 import (
-	b64 "encoding/base64"
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
-	"bytes"
-	"fmt"
 )
 
-var VERSION = "0.1.0"
-var MESSAGE = "Graphite Proxy instance - proxy graphite as a service"
+var version = "0.1.0"
+var description = "Graphite Proxy instance - proxy graphite as a service"
+var listen = "127.0.0.1"
+var port = 9999
+var graphiteAPIURL = "http://localhost:8888"
 
-const (
-	CONN_ROUTE = "/"
-	CONN_PORT = ":9999"
-)
+type MetricResponse struct {
+	Text          string `json:"text"`
+	Expandable    uint8  `json:"expandable"`
+	Leaf          uint8  `json:"leaf"`
+	ID            string `json:"id"`
+	AllowChildren uint8  `json:"allowChildren"`
+}
 
-func DecryptBasicAuth (sEnc string) (sDec []string) {
-	s := strings.Split(sEnc, " ")
-	temp, _ := b64.StdEncoding.DecodeString(s[1])
-	sDec = strings.Split(string(temp), ":")
+type MetricResponseError struct {
+	Errors map[string]string `json:"errors"`
+}
+
+func metricErrors(errorMessage string) string {
+	m := make(map[string]string, 1)
+	m["query"] = errorMessage
+	metricsErrors := MetricResponseError{
+		Errors: m,
+	}
+	jsn, _ := json.Marshal(metricsErrors)
+	log.Println("[ERROR]", errorMessage)
+	return string(jsn)
+}
+
+func handlerRender(w http.ResponseWriter, req *http.Request) {
+	client := &http.Client{}
+	login := req.Header.Get("login")
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+	// Restore the io.ReadCloser to its original state
+	req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	if err := req.ParseForm(); err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+
+	targets, ok := req.Form["target"]
+	if !ok {
+		http.Error(w, metricErrors("Targets not found"), 500)
+		return
+	}
+
+	for _, target := range targets {
+		// target is the metric + graphite functions
+		// ex: aliasByMetric(toto.webserver.prod.toto-web.tail-apache2.counter-status_2xx)
+		s := strings.Split(target, ".")
+		s1 := strings.Split(s[0], "(")
+		org := s1[len(s1)-1]
+
+		// if user request render with target (metric) that is not
+		// prefixed by his login, forbid request
+		if !strings.HasPrefix(org, login) {
+			log.Printf("401, Unauthorized (%s != %s)", org, login)
+			http.Error(w, "401, Unauthorized", 401)
+			return
+		}
+	}
+
+
+
+	url := fmt.Sprintf("%s/render", graphiteAPIURL)
+	r, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+	r.Header = req.Header
+	resp, err := client.Do(r)
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+	body, _ = ioutil.ReadAll(resp.Body)
+	fmt.Fprintf(w, string(body))
+	return
+
+}
+
+func handlerMetrics(w http.ResponseWriter, req *http.Request) {
+
+	client := &http.Client{}
+	login := req.Header.Get("login")
+
+	// Get query value ex: /metrics/find?query=login.collectd.*
+	// return "login.collectd.*"
+	query := req.URL.Query().Get("query")
+
+	// If query does not begin with login, return fake response
+	// with only one choice: the login
+	if !strings.HasPrefix(query, login) || query == "" {
+		fakeResponses := make([]MetricResponse, 1)
+		fakeResponses[0] = MetricResponse{
+			Text:          login,
+			Expandable:    1,
+			Leaf:          0,
+			ID:            login,
+			AllowChildren: 1,
+		}
+
+		body, err := json.Marshal(fakeResponses)
+		if err != nil {
+			http.Error(w, metricErrors(err.Error()), 500)
+			return
+		}
+
+		fmt.Fprintf(w, string(body))
+		return
+	}
+
+	// Login has now been forced in the query
+	url := fmt.Sprintf("%s/metrics/find?query=%s", graphiteAPIURL, query)
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+	r.Header = req.Header
+
+	// Do HTTP Request
+	resp, err := client.Do(r)
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+
+	// Read Response and return back
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, metricErrors(err.Error()), 500)
+		return
+	}
+
+	fmt.Fprintf(w, string(body))
 	return
 }
 
-func render(req *http.Request, login string, rw http.ResponseWriter, body []byte) {
-	s := strings.Split(req.FormValue("target"), ".")
-	s1 := strings.Split(s[0], "(")
-	org := s1[len(s1)-1]
-	if login == org {
-		client := &http.Client{}
-		r, err := http.NewRequest("POST", "http://localhost:8888/render", bytes.NewBuffer(body))
-		if err != nil {
-			panic(err)
+func handlerAuth(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, _, _ := r.BasicAuth()
+		if user == "" {
+			w.Header().Add("WWW-Authenticate", "Basic realm=\"Access Denied\"")
+			http.Error(w, "401, Unauthorized", 401)
+			return
 		}
-		r.Header = req.Header
-		resp, err := client.Do(r)
-		if err != nil {
-			panic(err)
-		}
-		body1, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(rw, string(body1))
+		r.Header.Set("login", user)
+		fn(w, r)
 	}
 }
 
-func metric(req *http.Request, login string, rw http.ResponseWriter, body []byte) {
-	s := strings.Split(req.RequestURI, "=")
-	if s[1] == "*" {
-		s1 := strings.Replace(req.RequestURI, "*", login, -1)
-		client := &http.Client{}
-		r, err := http.NewRequest("GET", fmt.Sprint("http://localhost:8888", s1), bytes.NewBuffer(body))
-		if err != nil {
-			panic(err)
+func handlerLog(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		login := "not authenticated"
+		if r.Header.Get("login") != "" {
+			login = r.Header.Get("login")
 		}
-		r.Header = req.Header
-		resp, err := client.Do(r)
-		if err != nil {
-			panic(err)
-		}
-		body1, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(rw, string(body1))
-	} else {
-		s1 := strings.Split(s[1], ".")
-		s2 := login
-		for i := 1; i < len(s1); i++ {
-			s2 = fmt.Sprint(s2, ".")
-			s2 = fmt.Sprint(s2, s1[i])
-		}
-		client := &http.Client{}
-		r, err := http.NewRequest("GET", fmt.Sprint("http://localhost:8888", fmt.Sprint(fmt.Sprint(s[0], "="), s2)), bytes.NewBuffer(body))
-		if err != nil {
-			panic(err)
-		}
-		r.Header = req.Header
-		resp, err := client.Do(r)
-		if err != nil {
-			panic(err)
-		}
-		body1, _ := ioutil.ReadAll(resp.Body)
-		fmt.Fprintf(rw, string(body1))
+		log.Printf("[INFO] %s %s %s%s (%s)", r.Method, r.Proto, r.Host, r.RequestURI, login)
+		fn(w, r)
 	}
 }
 
-func test(rw http.ResponseWriter, req *http.Request) {
-	if len(req.Header.Get("Authorization")) != 0 {
-		body, _ := ioutil.ReadAll(req.Body)
-		req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-		login := DecryptBasicAuth(req.Header.Get("Authorization"))
-		if req.URL.String() == "/render" {
-			render(req, login[0], rw, body)
-		} else {
-			metric(req, login[0], rw, body)
-		}
-	} else {
-		fmt.Fprintf(rw, "Unauthorized !")
+func handlerVersion(w http.ResponseWriter, _ *http.Request) {
+	var v = map[string]string{
+		"version":     version,
+		"description": description,
 	}
+
+	body, _ := json.Marshal(v)
+	w.Write(body)
+	return
 }
 
 func main() {
-	http.HandleFunc(CONN_ROUTE, test)
-	http.ListenAndServe(CONN_PORT, nil)
+	listen := fmt.Sprintf("%s:%d", listen, port)
+
+	http.HandleFunc("/metrics/find", handlerAuth(handlerLog(handlerMetrics)))
+	http.HandleFunc("/render", handlerAuth(handlerLog(handlerRender)))
+	http.HandleFunc("/version", handlerLog(handlerVersion))
+	log.Fatal(http.ListenAndServe(listen, nil))
+
 }
